48a7e175daebfa81599924cd7824123e6048c05e
neicx <alexceinrp@gmail.com>
2026-08-25 18:52:56 -0400

    chore: agregar gitignore y eliminar node,dist

--- backend/dist/src/auth/auth.utils.d.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-export declare function hash_password(password: string): Promise<string>;
-export declare function verify_password_hashed(storedHash: string, password: string): Promise<boolean>;
-export declare function signJwt(payload: Record<string, unknown>): string;
-export declare function verifyJwt(token: string): Record<string, unknown>;
--- backend/dist/src/auth/no-auth-required.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const NO_AUTH_REQUIRED_KEY = "noAuthRequired";
-export declare const NoAuthRequired: () => import("@nestjs/common").CustomDecorator<string>;
