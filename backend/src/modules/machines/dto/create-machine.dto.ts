d488321d642bb5536e913ff31aee2e25891ed757
neicx <alexceinrp@gmail.com>
2026-08-26 03:08:37 -0400

    feat: agregar dto de machines

---
+++ backend/src/modules/machines/dto/create-machine.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateMachineDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  type: string;
+
+}
